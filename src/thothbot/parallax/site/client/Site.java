2b6e069c2d40499048fbbc27cb3b629e242a43d2
thothbot <thothbot@gmail.com>
2012-10-17 12:58:35 +0400

    Initial import

---
+++ src/thothbot/parallax/site/client/Site.java
@@ -0,0 +1,129 @@
+package thothbot.parallax.site.client;
+
+import thothbot.parallax.core.client.AnimatedScene;
+import thothbot.parallax.core.client.RenderingPanel;
+import thothbot.parallax.core.client.events.AnimationReadyEvent;
+import thothbot.parallax.core.client.events.AnimationReadyHandler;
+import thothbot.parallax.core.client.events.Context3dErrorEvent;
+import thothbot.parallax.core.client.events.Context3dErrorHandler;
+import thothbot.parallax.core.client.events.SceneLoadingEvent;
+import thothbot.parallax.core.client.events.SceneLoadingHandler;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ToggleButton;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Site implements EntryPoint, AnimationReadyHandler, SceneLoadingHandler, Context3dErrorHandler
+{
+	public interface SiteResources extends ClientBundle
+	{
+		ImageResource play();
+		ImageResource pause();
+		ImageResource stop();
+	}
+	
+	/**
+	 * The static resources used throughout the Demo.
+	 */
+	public static final SiteResources resources = GWT.create(SiteResources.class);
+	private RenderingPanel renderingPanel;
+	
+	FlowPanel infoPanel;
+	private ToggleButton animationSwitch;
+	private Label infoText;
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() 
+	{
+		infoText = new Label("Loading...");
+		infoText.setStyleName("info");
+		animationSwitch = new ToggleButton();
+		animationSwitch.setEnabled(false);
+		animationSwitch.getUpFace().setImage(new Image(resources.stop()));
+
+		TerrainDynamic scene = new TerrainDynamic();
+		renderingPanel = new RenderingPanel();
+		RootPanel.get("animation").add(renderingPanel);
+		Window.addResizeHandler(new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent event) {
+				renderingPanel.onResize();
+
+			}
+		});
+
+		// Background color
+		renderingPanel.setBackground(0x111111);
+		renderingPanel.addAnimationReadyHandler(this);
+		renderingPanel.addSceneLoadingHandler(this);
+		renderingPanel.addCanvas3dErrorHandler(this);
+
+		renderingPanel.setAnimatedScene(scene.getDemoScene());
+		
+		infoPanel = new FlowPanel();
+		RootPanel.get("animationPanel").add(infoPanel);
+		infoPanel.add(animationSwitch);
+		infoPanel.add(infoText);
+	}
+	
+	/**
+	 * This event called when {@link RenderingPanel} is ready to animate a 
+	 * {@link AnimatedScene} in loaded example.
+	 */
+	public void onAnimationReady(AnimationReadyEvent event)
+	{
+		animationSwitch.setEnabled(true);
+		animationSwitch.setDown(true);
+		animationSwitch.getUpFace().setImage(new Image(resources.pause()));
+		animationSwitch.addClickHandler(new ClickHandler() {
+    		public void onClick(ClickEvent event) {
+    			if (animationSwitch.isDown())
+    			{
+    				animationSwitch.getUpFace().setImage(new Image(resources.pause()));
+    				Site.this.renderingPanel.getAnimatedScene().run();
+    			}
+    			else
+    			{
+    				animationSwitch.getUpFace().setImage(new Image(resources.play()));
+    				Site.this.renderingPanel.getAnimatedScene().stop();
+    			}
+    		}
+    	});
+	}
+
+	@Override
+	public void onSceneLoading(SceneLoadingEvent event) 
+	{
+		if(event.isLoaded())
+		{
+			infoText.setText("Your browser supports WebGL!");			
+		}
+		else
+		{
+			infoText.setText("Loading scene...");
+		}
+	}
+
+	@Override
+	public void onContextError(Context3dErrorEvent event) 
+	{
+		infoText.setText("Your browser does not support WebGL.");
+		RootPanel.get("animation").remove(renderingPanel);
+	}
+}
---
+++ src/thothbot/parallax/site/client/TerrainShader.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2012 Alex Usachev, thothbot@gmail.com
+ * 
+ * This file is part of Parallax project.
+ * 
+ * Parallax is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ * 
+ * Parallax is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Parallax. If not, see http://www.gnu.org/licenses/.
+ */
+
+package thothbot.parallax.site.client;
+
+import java.util.Arrays;
+import java.util.List;
+
+import thothbot.parallax.core.client.shaders.ChunksFragmentShader;
+import thothbot.parallax.core.client.shaders.ChunksVertexShader;
+import thothbot.parallax.core.client.shaders.Shader;
+import thothbot.parallax.core.client.shaders.Uniform;
+import thothbot.parallax.core.client.shaders.UniformsLib;
+import thothbot.parallax.core.shared.core.Color;
+import thothbot.parallax.core.shared.core.Vector2;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * Dynamic terrain shader<br>
+ * - Blinn-Phong<br>
+ * - height + normal + diffuse1 + diffuse2 + specular + detail maps<br>
+ * - point, directional and hemisphere lights (use with "lights: true" material option)<br>
+ * - shadow maps receiving
+ * <p>
+ * Based on three.js code
+ * 
+ * @author thothbot
+ *
+ */
+public final class TerrainShader extends Shader 
+{
+
+	interface Resources extends DefaultResources
+	{
+		Resources INSTANCE = GWT.create(Resources.class);
+		
+		@Source("terrain.vs")
+		TextResource getVertexShader();
+
+		@Source("terrain.fs")
+		TextResource getFragmentShader();
+	}
+
+	public TerrainShader() 
+	{
+		super(Resources.INSTANCE);
+	}
+
+	@Override
+	protected void initUniforms()
+	{
+		this.setUniforms(UniformsLib.getFog());
+		this.setUniforms(UniformsLib.getLights());
+		this.setUniforms(UniformsLib.getShadowmap());
+		this.addUniform("enableDiffuse1", new Uniform(Uniform.TYPE.I, false ));
+		this.addUniform("enableDiffuse2", new Uniform(Uniform.TYPE.I, false ));
+		this.addUniform("enableSpecular", new Uniform(Uniform.TYPE.I, false ));
+		this.addUniform("enableReflection", new Uniform(Uniform.TYPE.I, false ));
+
+		this.addUniform("tDiffuse1", new Uniform(Uniform.TYPE.T ));
+		this.addUniform("tDiffuse2", new Uniform(Uniform.TYPE.T ));
+		this.addUniform("tDetail", new Uniform(Uniform.TYPE.T ));
+		this.addUniform("tNormal", new Uniform(Uniform.TYPE.T ));
+		this.addUniform("tSpecular", new Uniform(Uniform.TYPE.T ));
+		this.addUniform("tDisplacement", new Uniform(Uniform.TYPE.T ));
+		
+		this.addUniform("uNormalScale", new Uniform(Uniform.TYPE.F, 1.0 ));
+		
+		this.addUniform("uDisplacementBias", new Uniform(Uniform.TYPE.F, 0.0 ));
+		this.addUniform("uDisplacementScale", new Uniform(Uniform.TYPE.F, 1.0 ));
+
+		this.addUniform("uDiffuseColor", new Uniform(Uniform.TYPE.C, new Color(0xeeeeee) ));
+		this.addUniform("uSpecularColor", new Uniform(Uniform.TYPE.C, new Color(0x111111) ));
+		this.addUniform("uAmbientColor", new Uniform(Uniform.TYPE.C, new Color(0x050505) ));
+
+		this.addUniform("uShininess", new Uniform(Uniform.TYPE.F, 30.0 ));
+		this.addUniform("uOpacity", new Uniform(Uniform.TYPE.F, 1.0 ));
+		
+		this.addUniform("uRepeatBase", new Uniform(Uniform.TYPE.V2, new Vector2(1, 1) ));
+		this.addUniform("uRepeatOverlay", new Uniform(Uniform.TYPE.V2, new Vector2(1, 1) ));
+
+		this.addUniform("uOffset", new Uniform(Uniform.TYPE.V2, new Vector2(0, 0) ));
+	}
+	
+	@Override
+	protected void updateFragmentSource(String src)
+	{
+		List<String> vars = Arrays.asList(
+				ChunksFragmentShader.SHADOWMAP_PARS,
+				ChunksFragmentShader.FOG_PARS
+		);
+			
+		List<String> main = Arrays.asList(
+				ChunksFragmentShader.SHADOWMAP,
+				ChunksFragmentShader.LINEAR_TO_GAMMA,
+				ChunksFragmentShader.FOG
+		);
+					
+		super.updateFragmentSource(Shader.updateShaderSource(src, vars, main));		
+	}
+	
+	@Override
+	protected void updateVertexSource(String src) 
+	{
+		List<String> vars = Arrays.asList(
+				ChunksVertexShader.SHADOWMAP_PARS
+		);
+			
+		List<String> main = Arrays.asList(
+				ChunksVertexShader.SHADOWMAP
+		);
+
+		super.updateVertexSource(Shader.updateShaderSource(src, vars, main));
+	}
+}
